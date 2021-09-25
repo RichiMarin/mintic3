68b5c20e81bc6c0b164ac99d8effeca6fbcc4aee
RichiMarin <ricardmaherrera@gmail.com>
2021-09-25 10:56:02 -0500

    commit25sept

---
+++ ProyectoCiclo3.App/ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioUsuario.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using ProyectoCiclo3.App.Dominio;
+using System.Linq;
+using System;
+ 
+namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
+{
+    public class RepositorioUsuario
+    {
+        List<Usuario> usuarios;
+ 
+    public RepositorioUsuario()
+        {
+            usuarios = new List<Usuario>()
+            {
+                new Usuario{id=1,nombre="Ricardo",apellidos= "Marin",direccion= "Calle 52",telefono= "88888888"},
+                new Usuario{id=2,nombre="Sebas",apellidos= "sanches",direccion= "calle 51",telefono= "999999"},
+                new Usuario{id=3,nombre="Magno",apellidos= "herrera",direccion= "calle50",telefono= "777777"}
+ 
+            };
+        }
+ 
+        public IEnumerable<Usuario> GetAll()
+        {
+            return usuarios;
+        }
+ 
+        public Usuario GetUsuarioWithId(int id){
+            return usuarios.SingleOrDefault(b => b.id == id);
+        }
+    }
+}
